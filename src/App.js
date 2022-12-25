978f6278755bca5b2bc9e2f1c65ad2579f4258f9
monkeycs60 <clement.serizay@gmail.com>
2022-12-26 00:54:00 +0100

    mise en forme header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
-import HomePage from './pages/HomePage/HomePage';
-import AboutPage from './pages/AboutPage/AboutPage';
+import "./App.css";
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/about" element={<AboutPage />} />
-          <Route element={<NotFoundPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/about" element={<AboutPage />} />
+        <Route path="/error404" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to={"/error404"} />} />
+      </Routes>
+    </Layout>
   );
 };
 
