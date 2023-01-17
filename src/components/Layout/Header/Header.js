f7032cb2be5cab3d1733a10af5e897bbb337deae
monkeycs60 <clement.serizay@gmail.com>
2023-01-17 19:49:24 +0100

    simplification classname header

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -4,32 +4,24 @@ import "./Header.css";
 import { ReactComponent as LogoHeaderDesktop } from "../../../assets/LOGO-header-DESKTOP.svg";
 import { ReactComponent as LogoHeaderMobile } from "../../../assets/LOGO-header-MOBILE.svg";
 
+const classNameResolver = ({ isActive }) => {
+  return isActive ? "activeRoute" : undefined;
+};
+
 const Header = () => {
   return (
     <header>
-      <NavLink
-        to={"/"}
-        className={({ isActive }) => (isActive ? "activeRoute" : undefined)}
-      >
+      <NavLink to={"/"} className={classNameResolver}>
         <LogoHeaderDesktop id="Logo-Header-Desktop" />
       </NavLink>
-      <NavLink
-        to={"/"}
-        className={({ isActive }) => (isActive ? "activeRoute" : undefined)}
-      >
+      <NavLink to={"/"} className={classNameResolver}>
         <LogoHeaderMobile id="Logo-Header-Mobile" />
       </NavLink>
       <nav>
-        <NavLink
-          to={"/"}
-          className={({ isActive }) => (isActive ? "activeRoute" : undefined)}
-        >
+        <NavLink to={"/"} className={classNameResolver}>
           Accueil
         </NavLink>
-        <NavLink
-          to={"/about"}
-          className={({ isActive }) => (isActive ? "activeRoute" : undefined)}
-        >
+        <NavLink to={"/about"} className={classNameResolver}>
           A Propos
         </NavLink>
       </nav>
